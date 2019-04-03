95c5643e4a5e8e375a7651cf41dee68bb76ad416
Richard Joffray <rjoffray@richards-mbp.lan>
2019-04-03 12:12:39 -0700

    fixed local dotenv

--- src/server.js
+++ src/server.js
@@ -1,15 +1,13 @@
 
-require('dotenv')
+require('dotenv').config()
 const debug = process.env.NODE_ENV !== 'production'
 const shutdownTime = process.env.SHUTDOWN_TIME || '0 * * * * *'
 const timezone = process.env.TZ || 'America/Los_Angeles'
 const { join } = require('path')
 
 var schedule = require('node-schedule');
-var moment = require('moment-timezone');
 var platform = require(join(__dirname, 'platform'))
 
-var now = moment().tz(timezone).format('H:mm a')
 console.log('platform host : ', process.env.PLATFORM_HOST)
 console.log('shutdownTime : ', shutdownTime)
 
